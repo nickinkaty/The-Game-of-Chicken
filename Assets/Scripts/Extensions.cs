370bcc8ad8c34500308d52de60027ee4b792ae22
nickinkaty <46138376+nickinkaty@users.noreply.github.com>
2022-10-05 19:56:22 -0500

    tiles

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public static class Extensions
+{
+    private static LayerMask layerMask = LayerMask.GetMask("Default");
+
+    public static bool Raycast(this Rigidbody2D rigidbody, Vector2 direction)
+    {
+        if (rigidbody.isKinematic) // KINEMATIC OBJECT IS STATIONARY.
+        {
+            return false;
+        }
+
+        float radius = 0.10f;
+        float distance = 0.15f;
+
+        RaycastHit2D hit = Physics2D.CircleCast(rigidbody.position, radius, direction.normalized, distance, layerMask);
+        return hit.collider != null && hit.rigidbody != rigidbody;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     private new Rigidbody2D rigidbody;
@@ -9,8 +10,14 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 velocity;
     private float inputAxis;
 
-    public float moveSpeed = 8f;
+    public float moveSpeed = 3f;
+    public float maxJumpHeight = 5f;
+    public float maxJumpTime = 1f;
+    public float jumpForce => (2f * maxJumpHeight) / (maxJumpTime / 2f);
+    public float gravity => (-2f * maxJumpHeight) / Mathf.Pow(maxJumpTime / 2f, 2f);
 
+    public bool grounded { get; private set; }
+    public bool jumping { get; private set; }
 
     private void Awake()
     {
@@ -20,12 +27,46 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         HorizontalMovement();
+
+        grounded = rigidbody.Raycast(Vector2.down);
+
+        if (grounded)
+        {
+            GroundedMovement();
+        }
+
+        ApplyGravity();
     }
 
     private void HorizontalMovement()
     {
         inputAxis = Input.GetAxis("Horizontal");
         velocity.x = Mathf.MoveTowards(velocity.x, inputAxis * moveSpeed, moveSpeed * Time.deltaTime);
+    }
+
+    private void GroundedMovement()
+    {
+        // prevent gravity from infinitly building up
+        velocity.y = Mathf.Max(velocity.y, 0f);
+        jumping = velocity.y > 0f;
+
+        // perform jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            velocity.y = jumpForce;
+            jumping = true;
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        // check if falling
+        bool falling = velocity.y < 0f || !Input.GetButton("Jump");
+        float multiplier = falling ? 2f : 1f;
+
+        // apply gravity and terminal velocity
+        velocity.y += gravity * multiplier * Time.deltaTime;
+        velocity.y = Mathf.Max(velocity.y, gravity / 2f);
     }
 
     private void FixedUpdate()
