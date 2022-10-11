b9dd2fea91136a9c5ad9a92f681cd761f242d4e1
randymesso <Randymesso@gmail.com>
2022-10-11 04:11:34 -0500

    Penguin AI Movement

---
+++ Assets/Scripts/Penguin_Movement.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Penguin_Movement : MonoBehaviour
+{
+    [SerializeField]
+    Transform player;
+
+    [SerializeField]
+    float agroRange;
+
+    [SerializeField]
+    float moveSpeed;
+
+    Rigidbody2D rb2d;
+
+    private bool facingRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //distance to player
+        float distToPlayer = Vector2.Distance(transform.position, player.position);
+        
+        if (distToPlayer < agroRange)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            StopChasingPlayer();
+        }
+        
+
+    }
+    void ChasePlayer()
+    {
+        if(transform.position.x < player.position.x)
+        {
+            //ememy is to the left side of the player, move right
+            rb2d.velocity = new Vector2(moveSpeed, 0);
+            transform.localScale = new Vector2(-1,1);
+            //transform.localScale = new Vector3(-1,1,1);
+            //Flip();
+            if(transform.position.y > player.position.y)
+            {
+                rb2d.velocity = new Vector2(moveSpeed, -moveSpeed);
+            }
+        }
+        else
+        {
+            //ememy is to the right side of the player, move right
+            rb2d.velocity = new Vector2(-moveSpeed,0);
+            transform.localScale = new Vector2(1,-1);
+            //transform.localScale = new Vector3(1,1,1);
+            //Flip();
+            if(transform.position.y > player.position.y)
+            {
+                rb2d.velocity = new Vector2(-moveSpeed, -moveSpeed);
+            }
+        }
+        
+    }
+
+    void StopChasingPlayer()
+    {
+        rb2d.velocity = new Vector2(0,0);
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0,180,0);
+    }
+    
+    
+}
+
---
+++ Assets/Scripts/Penguin_Movement_Platform.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Penguin_Movement_Platform : MonoBehaviour
+{
+    [Header("For Petrolling")]
+    [SerializeField] float moveSpeed;
+    private float moveDirection = 1;
+    private bool facingRight = true;
+    [SerializeField] Transform groundCheckPoint;
+    [SerializeField] float circleRadius;
+    [SerializeField] LayerMask groundLayer;
+    private bool checkingGround;
+    [Header("Other")]
+    private Rigidbody2D enemyRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        checkingGround = Physics2D.OverlapCircle(groundCheckPoint.position,circleRadius,groundLayer);
+        Petrolling();
+    }
+
+    void Petrolling()
+    {
+        if (!checkingGround)
+        {
+            Flip();
+        }
+        enemyRB.velocity = new Vector2(moveSpeed * moveDirection, enemyRB.velocity.y);
+    }
+
+    void Flip()
+    {
+        moveDirection *= -1;
+        facingRight = !facingRight;
+        transform.Rotate(0,180,0);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(groundCheckPoint.position,circleRadius);
+    }
+}
