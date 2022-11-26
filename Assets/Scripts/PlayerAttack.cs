23fe92154e6c215e53a3f61aeb3f07c0e783db7b
randymesso <Randymesso@gmail.com>
2022-11-25 22:53:09 -0600

    Jump/Fireball key changes

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -17,7 +17,7 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1) && energySystem.currentEnergy >= fireballEnergy)
+        if (Input.GetKeyDown(KeyCode.Space) && energySystem.currentEnergy >= fireballEnergy)
         {
             AudioManagerScript.PlaySound("fireball");
             energySystem.LoseEnergy(fireballEnergy);
